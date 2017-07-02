187e068e7d944095cbe909c71d1136d6d24aaa9b
Isaben <isabenjuda@gmail.com>
2017-07-02 05:25:34 -0300

    scrapper aplicado ao django, AGORA FUNCIONA!

--- newsite/views.py
+++ newsite/views.py
@@ -2,11 +2,27 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-import json
+import scrapy
+#from scrapy.crawler import CrawlerRunner
+from scraper.jornais.spiders.folha import FolhaSpider
+from scraper.jornais.spiders.economia import EconomiaSpider
 from .models import Noticias
 from datetime import datetime
-# Create your views here.
+from twisted.internet import reactor
 
+from scrapy import signals
+from scrapy.crawler import CrawlerRunner
+# Create your views here.
+import scrapydo
+scrapydo.setup()
+
+scrapydo.default_settings.update({
+    "USER_AGENT": 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+    "ROBOTSTXT_OBEY": True,
+    "ITEM_PIPELINES": {
+        'scraper.jornais.pipelines.JornalPipeline': 300,
+    },
+})
 """def index(request):
     return HttpResponse("VAI TOMAR NO CU")
     """
@@ -15,31 +31,27 @@ class IndexView(generic.ListView):
     template_name = "newsite/index.html"
     context_object_name = "news"
 
+
+    """runner = CrawlerRunner(settings = {
+        "USER_AGENT": 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+        "ROBOTSTXT_OBEY": True,
+        "ITEM_PIPELINES": {
+            'scraper.jornais.pipelines.JornalPipeline': 300,
+        },
+
+
+        })
+
+    d = self.runner.crawl(FolhaSpider)
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run(installSignalHandlers = 0) # the script will block here until the crawling is finished"""
+    
     def get_queryset(self):
+        scrapydo.run_spider(FolhaSpider)
+
         return Noticias.objects.order_by("-date")
 
 class DetailView(generic.DetailView):
     model = Noticias
     template_name = "newsite/detail.html"    
     context_object_name = "news"
-
-def update_list():
-
-    data = open("newsite/result.json", 'r')
-    temp = ""
-    for line in data:
-        temp += line
-    
-    parsed = json.loads(temp)
-
-    for news in parsed:
-        title = news["Title"].replace("\t", "").replace("\n", "")
-        image_link = news["image_link"]
-        if(image_link == None):
-            image_link = "http://dhiglobal.com/wp-content/uploads/2016/07/placeholder.jpg"
-        date = datetime.strptime(news["date"], "%Y-%m-%d %H:%M")
-        article = news["article"]
-        link = news["link"]
-        source = news["fonte"]
-        temp = Noticias(title = title, image_link = image_link, date = date, article = article, link = link, fonte = source)
-        temp.save()
